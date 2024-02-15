c8c07c0e891210ef631049b38b4ce54003a2ec96
wldnd9904 <wldnd9904@uos.ac.kr>
2024-02-15 16:10:48 +0900

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by 최지웅 on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
